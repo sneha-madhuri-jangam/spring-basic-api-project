76efbf1e2cf8c834e90de82df089471e10fe0141
sneha-madhuri-jangam <snehamadhurijangam2002@gmail.com>
2025-09-03 17:28:31 +0530

    second

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+// import org.springframework.boot.web.servlet.FilterRegistrationBean;
+// import org.springframework.boot.web.servlet.error.ErrorPageFilter;
+// import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer{
@@ -12,4 +14,14 @@ public class DemoApplication extends SpringBootServletInitializer{
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	// Prevent ErrorPageFilter conflict when deploying to external Tomcat
+    // @Bean
+    // public FilterRegistrationBean<ErrorPageFilter> disableSpringBootErrorFilter(ErrorPageFilter filter) {
+    //     FilterRegistrationBean<ErrorPageFilter> registration = new FilterRegistrationBean<>(filter);
+    //     registration.setEnabled(false);
+    //     return registration;
+    // }
+
 }
+
+
