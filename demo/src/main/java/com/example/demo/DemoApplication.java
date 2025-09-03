14bfb1068784b0b290c479414b5cb9dfce773f50
sneha-madhuri-jangam <snehamadhurijangam2002@gmail.com>
2025-09-03 19:18:21 +0530

    second-final-afterchanging pom file2-3

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 // import org.springframework.boot.web.servlet.FilterRegistrationBean;
 // import org.springframework.boot.web.servlet.error.ErrorPageFilter;
